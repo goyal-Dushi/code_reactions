25186ce0024d9333b570b331af4e9655dbd70144
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-10-14 16:44:55 +0530

    provided store to all components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import NotesSection from "./modules/Notes.module";
 import SubjectSection from "./modules/Subject.module";
 import TopicSection from "./modules/Topics.module";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path={"/"} exact component={SubjectSection} />
-        <Route path={"/subject/:paramID"} component={TopicSection} />
-        <Route path={"/topics/:subjectID/:topicID"} component={NotesSection} />
+        <Provider store={store}>
+          <Route path={"/"} exact component={SubjectSection} />
+          <Route path={"/subject/:paramID"} component={TopicSection} />
+          <Route
+            path={"/topics/:subjectID/:topicID"}
+            component={NotesSection}
+          />
+        </Provider>
       </Switch>
     </Router>
   );
