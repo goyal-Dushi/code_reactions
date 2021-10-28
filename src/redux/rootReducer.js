c71f69b4af9be8b0ac6c4c6d83d7acf1e0ff96ac
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-10-28 18:09:13 +0530

    included topic and notes reducer in store

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import subjectReducer from "./subjectSlice";
+import topicReducer from "./topicsSlice";
+import noteReducer from "./notesSlice";
 
 export const rootReducer = combineReducers({
   subjects: subjectReducer,
+  topics: topicReducer,
+  notes: noteReducer,
 });
